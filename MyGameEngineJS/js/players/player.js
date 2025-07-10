6363313bce16197631fa8eddb3079f0906928aee
Mark Mavis <legomavrock@gmail.com>
2025-07-10 10:26:44 -0500

    finished up first part

--- MyGameEngineJS/js/players/player.js
+++ MyGameEngineJS/js/players/player.js
@@ -1,3 +1,7 @@
+var keys = require('../utils/utils.keysDown.js'),   mathHelpers = require('../utils/utils.math.js');
+
+
+
 /** Player Module
  * Main player entity module.
  */
@@ -31,7 +35,24 @@ function Player(scope, x, y) {
     // Mutates state as needed for proper rendering next state
     player.update = function playerUpdate() {
         // Check if keys are pressed, if so, update the players position.
+        if (keys.isPressed.left) {
+            player.state.position.x -= player.state.moveSpeed;
+        }
+
+        if (keys.isPressed.right) {
+            player.state.position.x += player.state.moveSpeed;
+        }
+
+        if (keys.isPressed.up) {
+            player.state.position.y -= player.state.moveSpeed;
+        }
+
+        if (keys.isPressed.down) {
+            player.state.position.y += player.state.moveSpeed;
+        }
         // Bind the player to the boundary
+        player.state.position.x = player.state.position.x.boundary(0, (scope.constants.width - width));
+        player.state.position.y = player.state.position.y.boundary(0, (scope.constants.height - height));
     };
 
     return player;
